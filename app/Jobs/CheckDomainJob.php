adc48a5285376d837dc49dcb84bde7d5c002aec4
feycot <feycot@gmail.com>
2020-03-29 12:51:09 +0500

    fix lint

--- app/Jobs/CheckDomainJob.php
+++ app/Jobs/CheckDomainJob.php
@@ -12,7 +12,10 @@ use DB;
 
 class CheckDomainJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     private string $domain;
 
